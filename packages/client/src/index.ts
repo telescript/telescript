b94a9d076aa9015e05f1b9263b6777e745e4d8ed
Synbulat Biishev <me@syjalo.dev>
2026-05-22 22:14:41 +0500

    feat: add structure exports

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,1 +1,2 @@
 export * from './client/Client.js';
+export * from './structures';
---
+++ packages/client/src/structures/index.ts
@@ -0,0 +1,4 @@
+export * from './Chat.js';
+export * from './Message.js';
+export * from './Structure.js';
+export * from './User.js';
