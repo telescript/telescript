189aa5eb1795224dba8fc754328bc7cff9f48020
Synbulat Biishev <me@syjalo.dev>
2026-05-25 20:48:41 +0500

    feat: new message properties

---
+++ packages/client/src/structures/DirectMessagesTopic.ts
@@ -0,0 +1,13 @@
+import { APIDirectMessagesTopic } from '@telescript/api-types';
+import { Structure } from './Structure';
+
+export class DirectMessagesTopic extends Structure<APIDirectMessagesTopic> {
+	public get id() {
+		return this[Structure.DataProperty].topic_id;
+	}
+
+	public get user() {
+		const data = this[Structure.DataProperty].user;
+		return data ? this.client.users.resolve(data) : null;
+	}
+}
--- packages/client/src/structures/Message.ts
+++ packages/client/src/structures/Message.ts
@@ -1,24 +1,58 @@
 import { APIMessage } from '@telescript/api-types';
 import { Structure } from './Structure.js';
-import { User } from './User.js';
-import { Chat } from './Chat.js';
+import { DirectMessagesTopic } from './DirectMessagesTopic.js';
 
 export class Message extends Structure<APIMessage> {
 	public get id() {
 		return this[Structure.DataProperty].message_id;
 	}
 
+	public get threadId() {
+		return this[Structure.DataProperty].message_thread_id;
+	}
+
+	public get directMessagesTopic() {
+		const data = this[Structure.DataProperty].direct_messages_topic;
+		return data ? new DirectMessagesTopic(this.client, data) : null;
+	}
+
 	public get from() {
-		const from = this[Structure.DataProperty].from;
-		return from ? new User(this.client, from) : null;
+		const data = this[Structure.DataProperty].from;
+		return data ? this.client.users.resolve(data) : null;
+	}
+
+	public get senderChat() {
+		const data = this[Structure.DataProperty].sender_chat;
+		return data ? this.client.chats.resolve(data) : null;
+	}
+
+	public get senderBoostCount() {
+		return this[Structure.DataProperty].sender_boost_count ?? null;
+	}
+
+	public get senderBusinessBot() {
+		const data = this[Structure.DataProperty].sender_business_bot;
+		return data ? this.client.users.resolve(data) : null;
+	}
+
+	public get senderTag() {
+		return this[Structure.DataProperty].sender_tag ?? null;
 	}
 
 	public get date() {
 		return this[Structure.DataProperty].date;
 	}
 
+	public get guestQueryId() {
+		return this[Structure.DataProperty].guest_query_id ?? null;
+	}
+
+	public get businessConnectionId() {
+		return this[Structure.DataProperty].business_connection_id ?? null;
+	}
+
 	public get chat() {
-		return new Chat(this.client, this[Structure.DataProperty].chat);
+		return this.client.chats.resolve(this[Structure.DataProperty].chat);
 	}
 
 	public get text() {
