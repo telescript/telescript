7f917a127f64ebd396efb4c7b0787ded1982c84f
Synbulat Biishev <me@syjalo.dev>
2026-06-03 23:33:54 +0500

    fix: add exports

--- packages/client/src/client/updates/handlers/index.ts
+++ packages/client/src/client/updates/handlers/index.ts
@@ -1,2 +1,7 @@
+export * from './business-message.js';
+export * from './channel-post.js';
+export * from './edited-business-message.js';
+export * from './edited-channel-post.js';
 export * from './edited-message.js';
+export * from './guest-message.js';
 export * from './message.js';
