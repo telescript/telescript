c56015f138e568df40e397eae496089b6436dbdf
Synbulat Biishev <me@syjalo.dev>
2026-06-05 01:32:53 +0500

    refactor(SendTextAction): use data property

--- packages/client/src/structures/chats/actions/SendTextAction.ts
+++ packages/client/src/structures/chats/actions/SendTextAction.ts
@@ -1,6 +1,6 @@
 import { APIChat, ChatType } from '@telescript/api-types';
 import { Client } from '../../../client/index.js';
-import { BaseChat } from '@telescript/structures';
+import { BaseChat, Structure } from '@telescript/structures';
 
 export interface SendTextAction<
 	Type extends ChatType = ChatType,
@@ -11,6 +11,6 @@ export class SendTextAction {
 	declare public client: Client;
 
 	public async sendText(text: string) {
-		return this.client.chats.sendText(this.id, text);
+		return this.client.chats.sendText(this[Structure.DataProperty].id, text);
 	}
 }
