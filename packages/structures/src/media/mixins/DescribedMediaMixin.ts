1c36412030b5c49bda1bc3b068969949d573605a
Synbulat Biishev <me@syjalo.dev>
2026-06-05 18:17:40 +0500

    fix: change to camel case

--- packages/structures/src/media/mixins/DescribedMediaMixin.ts
+++ packages/structures/src/media/mixins/DescribedMediaMixin.ts
@@ -3,7 +3,7 @@ import { Structure } from '../../Structure.js';
 import { MIMETypedMediaMixin } from './MIMETypedMediaMixin.js';
 
 export class DescribedMediaMixin<Data extends MediaBase & DescribedMediaBase> extends MIMETypedMediaMixin<Data> {
-	public get file_name() {
+	public get fileName() {
 		return this[Structure.DataProperty].file_name ?? null;
 	}
 }
--- packages/structures/src/media/mixins/MIMETypedMediaMixin.ts
+++ packages/structures/src/media/mixins/MIMETypedMediaMixin.ts
@@ -3,7 +3,7 @@ import { BaseMedia } from '../BaseMedia.js';
 import { Structure } from '../../Structure.js';
 
 export class MIMETypedMediaMixin<Data extends MediaBase & MIMETypedMediaBase> extends BaseMedia<Data> {
-	public get mime_type() {
+	public get mimeType() {
 		return this[Structure.DataProperty].mime_type ?? null;
 	}
 }
