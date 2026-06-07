ba3b692b14f67005c8ab6c1664f3163129c0416d
Synbulat Biishev <me@syjalo.dev>
2026-06-07 23:35:35 +0500

    feat: better requester

--- packages/requester/src/constants.ts
+++ packages/requester/src/constants.ts
@@ -3,4 +3,5 @@ import type { RequesterOptions } from './Requester.js';
 export const DefaultRequesterOptions = {
 	api: 'https://api.telegram.org',
 	maxRetries: 3,
+	retryAfter: 3000,
 } as const satisfies RequesterOptions;
