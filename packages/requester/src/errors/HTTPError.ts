662c86258bf37a540f83d7924dde8474029386e6
Synbulat Biishev <me@syjalo.dev>
2026-06-08 02:32:28 +0500

    feat: declare error cause property

--- packages/requester/src/errors/HTTPError.ts
+++ packages/requester/src/errors/HTTPError.ts
@@ -1,6 +1,8 @@
 export class HTTPError extends Error {
 	public status: number;
 
+	declare cause: Response;
+
 	public constructor(res: Response) {
 		super(res.statusText, { cause: res });
 		this.status = res.status;
--- packages/requester/src/errors/TelegramAPIError.ts
+++ packages/requester/src/errors/TelegramAPIError.ts
@@ -3,6 +3,8 @@ import { APIErrorResponse } from '@telescript/api-types';
 export class TelegramAPIError extends Error {
 	public code: number;
 
+	declare cause: APIErrorResponse;
+
 	public constructor(data: APIErrorResponse) {
 		super(data.description, { cause: data });
 		this.code = data.error_code;
