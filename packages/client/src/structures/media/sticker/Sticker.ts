51e6effbc123cc3397df8736484eeac32dbc5979
Synbulat Biishev <me@syjalo.dev>
2026-07-15 16:49:59 +0500

    fix: use client `File` structure

--- packages/client/src/structures/media/sticker/Sticker.ts
+++ packages/client/src/structures/media/sticker/Sticker.ts
@@ -2,6 +2,7 @@ import { APISticker } from '@telescript/api-types';
 import { BaseMedia } from '../BaseMedia.js';
 import { Sticker as ParentSticker, Structure } from '@telescript/structures';
 import { MaskPosition } from './MaskPosition.js';
+import { File } from '../../file/File.js';
 
 export interface Sticker extends ParentSticker {}
 
@@ -12,7 +13,7 @@ export class Sticker extends BaseMedia<APISticker> {
 
 	public get premiumAnimation() {
 		const data = this[Structure.DataProperty].premium_animation;
-		return data ? new File(data) : null;
+		return data ? new File(this.client, data) : null;
 	}
 
 	public get maskPosition() {
